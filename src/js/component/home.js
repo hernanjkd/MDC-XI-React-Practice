0c8f024e19c31c01e555b0758bcad075a8aff02b
hernanjkd <hernanjkd@gmail.com>
2020-02-12 17:18:21 -0500

    card project

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,42 +1,70 @@
 import React, { useState } from "react";
+import { Navbar } from "./navbar";
+import { Card } from "./card";
+
+//include images into your bundle
+import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 export function Home() {
 	const [list, setList] = useState([]);
-	const [content, setContent] = useState();
 
+	const cards = {
+		suits: [1, 2, 3, 4],
+		value: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+	};
+	const repeats = (suit, value, arr) => {
+		return arr.find(e => e.suit === suit && e.value === value) === undefined
+			? false
+			: true;
+	};
+	const setSuit = num =>
+		num === 1 ? "♠" : num === 2 ? "♥" : num === 3 ? "♣" : "♦";
+	const setValue = num =>
+		num === 1
+			? "A"
+			: num === 11
+				? "J"
+				: num === 12
+					? "Q"
+					: num === 13
+						? "K"
+						: num;
+	const ran = range => Math.floor(Math.random() * range);
+	function deal(q) {
+		let arr = [];
+
+		for (let i = 0; i < q; i++) {
+			let suit = cards.suits[ran(4)];
+			let value = cards.value[ran(13)];
+
+			while (repeats(suit, value, arr)) {
+				suit = cards.suits[ran(4)];
+				value = cards.value[ran(13)];
+			}
+
+			arr.push({
+				suit: setSuit(suit),
+				value: setValue(value)
+			});
+		}
+
+		setList(arr);
+	}
+	console.log(list);
 	return (
 		<>
-			<div className="text-center mt-5 pt-5">
-				<button
-					className="anything text-light"
-					onClick={() => {
-						setList(list.concat(content));
-						setContent("");
-					}}>
-					Add To Do
-				</button>
-				<input
-					value={content}
-					onChange={e => setContent(e.target.value)}
-					onKeyPress={e => {
-						if (e.key === "Enter") {
-							setList(list.concat(content));
-							setContent("");
-						}
-					}}
-				/>
-				{list.map((item, index) => {
-					return (
-						<div
-							key={index}
-							onClick={() =>
-								setList(list.filter(e => e !== item))
-							}>
-							{item}
-						</div>
-					);
-				})}
+			<div
+				className="poker d-inline-block border bg-light p-1 rounded-sm ${color}"
+				onClick={() => deal(10)}>
+				<div className="h-50 d-flex justify-content-between">
+					<span>3</span>
+					<span>♠</span>
+				</div>
+				<div className="rotate align-self-end h-50 d-flex justify-content-between">
+					<span>3</span>
+					<span>♠</span>
+				</div>
 			</div>
 		</>
 	);
---
+++ src/js/component/home4.js
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+
+//create your first component
+export function Home() {
+	const [list, setList] = useState([]);
+	const [content, setContent] = useState();
+
+	return (
+		<>
+			<div className="text-center mt-5 pt-5">
+				<button
+					className="anything text-light"
+					onClick={() => {
+						setList(list.concat(content));
+						setContent("");
+					}}>
+					Add To Do
+				</button>
+				<input
+					value={content}
+					onChange={e => setContent(e.target.value)}
+					onKeyPress={e => {
+						if (e.key === "Enter") {
+							setList(list.concat(content));
+							setContent("");
+						}
+					}}
+				/>
+				{list.map((item, index) => {
+					return (
+						<div
+							key={index}
+							onClick={() =>
+								setList(list.filter(e => e !== item))
+							}>
+							{item}
+						</div>
+					);
+				})}
+			</div>
+		</>
+	);
+}
